1757e4c768e4fd74d60fcf84170dbcf9bdf1b409
nekqy <nekqy@yandex.ru>
2016-11-10 01:39:53 +0300

    может так будет аудио стабильно отправляться

--- app/app.js
+++ app/app.js
@@ -91,8 +91,6 @@ define([
 
         }]);
 
-        angular.element(function() {
-            angular.bootstrap(document, ['soundcrumbs']);
-        });
+        angular.bootstrap(document, ['soundcrumbs']);
     });
 });
