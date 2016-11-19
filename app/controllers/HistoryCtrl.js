a4b0131db7c0fb9058198907e976de1a026ff9ab
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2016-11-19 11:40:42 +0300

    Finish History controller

--- app/controllers/HistoryCtrl.js
+++ app/controllers/HistoryCtrl.js
@@ -1,12 +1,31 @@
 define([], function () {
 	function HistoryCtrl($scope, VKApi) {
-		var ref = firebase.database().ref('History');
+		var refHistory = firebase.database().ref('History');
+		var refRecords = firebase.database().ref('SoundCrumbs');
 
-		/* FIREBASE */
-		function getHistory() {
+		$scope.historyList = [];
 
-		}
-		/* FIREBASE END */
+		VKApi.getSession().then(function(session) {
+			refHistory.orderByChild('uid').equalTo(session.mid).orderByChild('date').on('value', function(snapHist) {
+				snapHist.forEach(function (dataHist) {
+					var audio = {};
+					var track = dataHist.val();
+
+					audio.dateStr = new Date(track.date).toLocaleDateString();
+
+					refRecords.orderByChild('sound').equalTo(track.sound).on('value', function(snapRecord) {
+						snapRecord.forEach(function (dataRecord) {
+							var trackData = dataRecord.val();
+
+							audio.description = trackData.description;
+							audio.rating = trackData.rating;
+						});
+					});
+
+					$scope.historyList.push(audio);
+				});
+			});
+		});
 	}
 
 	return HistoryCtrl;
