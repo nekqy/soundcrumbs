5b4eb75374c09f07ac134253b00ad336040ed854
nxtSwitch <nxtSwitch@gmail.com>
2016-11-19 19:24:48 +0300

    record popup fix

--- app/controllers/RecordAudioCtrl.js
+++ app/controllers/RecordAudioCtrl.js
@@ -202,11 +202,12 @@ define([], function() {
 
       $scope.cancelRecord = function() {
         $('#l_modal').hide();
-        console.log('hide');
+        $scope.goToBack();
       };
 
       $scope.sendAudio = function() {
         var description =  $('#sendAudio').val();
+        var res = $scope.tmp_res;
 
         console.log('description = ' + description);
 
@@ -259,6 +260,9 @@ define([], function() {
            var addingAudio = $scope.audioList.find(function(val) {
               return val.aid === audioId;
            });
+
+            $scope.tmp_res = res;
+
            var defaultDescription = addingAudio ? addingAudio.artist + ' - ' + addingAudio.title : '';
            $('.b_form__input').val(defaultDescription);
            $('#l_modal').show();
