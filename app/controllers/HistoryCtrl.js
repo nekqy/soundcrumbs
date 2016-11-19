21675a4eca6193746baf4212df4f28f1c6235d57
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2016-11-19 13:14:01 +0300

    Finish History

--- app/controllers/HistoryCtrl.js
+++ app/controllers/HistoryCtrl.js
@@ -33,6 +33,8 @@ define([], function () {
 
 							audio.description = trackData.description;
 							audio.rating = Object.keys(trackData.liked || {}).length - Object.keys(trackData.disliked || {}).length;
+
+							audio.shareUrl = getShareUrl(trackData);
 						});
 					});
 
@@ -40,6 +42,24 @@ define([], function () {
 				});
 			});
 		});
+
+		function getShareUrl(data) {
+			var title; 
+			if (data.description)
+				title = "Прийди туда и послушай " + data.description + " в SoundCrumbs";
+			else
+				title = "Прийди туда и послушай запись в SoundCrumbs";
+			title = encodeURIComponent(title);
+
+			var image = encodeURIComponent('http://static-maps.yandex.ru/1.x/?lang=en-US&ll=' + data.coord_x +
+						',' + data.coord_y + '&z=13&l=map&size=600,300&pt='+ data.coord_x +
+						',' + data.coord_y +',pmgrm');
+			var link = encodeURIComponent('http://www.openstreetmap.org/?mlat=' + data.coord_y + '&mlon=' + data.coord_x + 
+					   '&zoom=18#map=18/' + data.coord_y + '/' + data.coord_x);
+
+			return "https://vk.com/share.php?url=" + link + "&title=" + title + "&description=" + title +
+				   "&image=" + image + "&noparse=false";
+		}
 
 	    $scope.goToBack = function()
 	    {
