1e029507f76632a026387cc6fa15176aaddbb9c1
Vyacheslav Pukhanov <vyacheslav.pukhanov@gmail.com>
2016-11-19 10:07:57 +0300

    History controller barebones

--- app/controllers/HistoryCtrl.js
+++ app/controllers/HistoryCtrl.js
@@ -1,6 +1,12 @@
 define([], function () {
 	function HistoryCtrl($scope, VKApi) {
+		var ref = firebase.database().ref('History');
 
+		/* FIREBASE */
+		function getHistory() {
+
+		}
+		/* FIREBASE END */
 	}
 
 	return HistoryCtrl;
